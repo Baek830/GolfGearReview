c8854d9f1b8eb54920f2216c44c2f1e9598791e3
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-09-19 14:19:17 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,21 +2,20 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'  #=> 追加
-    config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => ENV['AWS_S3_REGION'],     # 例（東京）: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AWS_IAM_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_IAM_ACCESS_KEY']
-    }
-    config.fog_directory     =  ENV['AWS_S3_BUCKET']
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/golfgearreview'
-    # キャッシュの保存期間
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-  end
-  # 日本語ファイル名の設定
-  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
+CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.test?
+    config.storage = :file
+  # else
+  #   config.storage = :fog
+  #   config.fog_provider = 'fog/aws'  #=> 追加
+  #   config.fog_credentials = {
+  #     # Amazon S3用の設定
+  #     :provider: 'AWS',
+  #     :region: 'ap-northeast-1',
+  #     :aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+  #     :aws_secret_access_key => ENV['AWS_IAM_ACCESS_KEY']
+  #   }
+  #   config.fog_directory = ''
+  #   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/golfgearreview'
+  # end
 end
