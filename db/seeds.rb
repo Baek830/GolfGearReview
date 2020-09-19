274b2e2ab77eaa89977eeb90bf625bd6b427eeb2
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-09-19 15:09:36 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create!(
+Club.create!(
   [
     {name: 'Driver'},
     {name: 'Fairwaywood'},
