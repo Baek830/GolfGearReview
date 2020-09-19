e91454a6cf80ec2a4382bb94b8a830137cecce7b
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-09-19 15:07:40 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: 'aa@aa',
+  name: 'Hideki Matsuyama',
+)
+
+Category.create!(
+  [
+    {name: 'Driver'},
+    {name: 'Fairwaywood'},
+    {name: 'Utility'},
+    {name: 'Iron'},
+    {name: 'Wedge'},
+    {name: 'Putter'},
+    {name: 'Ball'}
+  ]
+)
+Maker.create!(
+  [
+    {name: 'Taylormade'},
+    {name: 'Calloway'},
+    {name: 'SRIXON'},
+    {name: 'XXIO'},
+    {name: 'PING'},
+    {name: 'Titleist'},
+  ]
+)
