d5db5e70ed13b1a181ae5957baf96517778fa5fc
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-09-19 14:48:29 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,17 +5,17 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.test?
     config.storage = :file
-  # else
-  #   config.storage = :fog
-  #   config.fog_provider = 'fog/aws'  #=> 追加
-  #   config.fog_credentials = {
-  #     # Amazon S3用の設定
-  #     :provider: 'AWS',
-  #     :region: 'ap-northeast-1',
-  #     :aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
-  #     :aws_secret_access_key => ENV['AWS_IAM_ACCESS_KEY']
-  #   }
-  #   config.fog_directory = ''
-  #   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/golfgearreview'
-  # end
+  else
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'  #=> 追加
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory = 'golfgearreview'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/golfgearreview'
+  end
 end
